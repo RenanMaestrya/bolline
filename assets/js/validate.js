a6c877a004c7ccdae2ec5aee9e94719596c7529b
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-08-27 23:44:26 -0300

    VERIFICAÇÃO CONCLUIDA!!!

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,23 +1,23 @@
 let pageAnswer = document.getElementsByClassName('decif').item(HTMLCollection).innerHTML;
 
 const btn = document.querySelector('#button');
-const aw = document.querySelector('#answer');
+
+const formC = document.querySelector('.form-control');
 
 btn.addEventListener('click', function(e){
     e.preventDefault();
     const aw = document.querySelector('#answer');
-    const awValue = aw.value;
+    const awValue = aw.value.trim();
 
     if(awValue === pageAnswer){
         window.location.href = page;
     }else{
-        alert('errado');
+        formC.className = 'form-control error';
     }
 })
 
+const inputAw = document.querySelector('#answer');
 
-
-
-
-
-
+inputAw.addEventListener('keydown', function(){
+    formC.className = 'form-control';
+})
